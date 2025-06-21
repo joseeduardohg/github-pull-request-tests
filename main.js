82b0f356fe1e4e78bf1cb2129340fdc38905b8b7
joseeduardohg <101235315+joseeduardohg@users.noreply.github.com>
2025-06-21 00:44:00 -0500

    feat: add divergent commit to see what update with rebase does

--- main.js
+++ main.js
@@ -4,4 +4,4 @@ console.log("Hello, World!");
 console.log("A partir de aquí, empezaré las pruebas con git rebase");
 
 console.log('Hola desde merge-with-rebase');
-console.log('Hola otra vez desde merge-with-rebase');
+console.log('Hola otra vez desde merge-with-rebase'); // Este es un comentario de una línea
