557f19b8b1e584c25b70a23b07560907e5f294c6
joseeduardohg <101235315+joseeduardohg@users.noreply.github.com>
2025-06-21 00:42:44 -0500

    feat: add first line in diverged-with-rebase

--- main.js
+++ main.js
@@ -5,3 +5,5 @@ console.log("A partir de aquí, empezaré las pruebas con git rebase");
 
 console.log('Hola desde merge-with-rebase');
 console.log('Hola otra vez desde merge-with-rebase'); // Este es un comentario de una línea
+
+console.log('Hola desde diverged-with-rebase');
