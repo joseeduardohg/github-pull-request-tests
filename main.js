79803f526ef74568cc51e9d41009466a2b0fd53f
joseeduardohg <101235315+joseeduardohg@users.noreply.github.com>
2025-06-21 00:06:59 -0500

    feat: add second line in merge-with-rebase

--- main.js
+++ main.js
@@ -4,3 +4,4 @@ console.log("Hello, World!");
 console.log("A partir de aquí, empezaré las pruebas con git rebase");
 
 console.log('Hola desde merge-with-rebase');
+console.log('Hola otra vez desde merge-with-rebase');
